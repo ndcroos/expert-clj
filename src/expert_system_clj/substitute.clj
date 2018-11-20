d2d1cb50fc98999d861e6efd35ff4121a9250829
Nick Decroos <ndcroos@users.noreply.github.com>
2018-11-20 20:50:53 +0100

    Update substitute.clj

--- src/expert_system_clj/substitute.clj
+++ src/expert_system_clj/substitute.clj
@@ -1,12 +1,14 @@
-(defn Substitute [s expression]
-(cond ((or (null expression)
-(null s)) expression)
-((Singleton? expression)
-(let ((binding nil))
-(if (Variable? expression)
-(setq binding (LookUp expression s)))
-(if (null binding)
-expression
-(Term binding))))
-(t (cons (Substitute s (first expression))
-(Substitute s (rest expression))))))
+(defn Substitute 
+    [s expression]
+    (cond 
+        ((or (null expression)
+        (null s)) expression)
+    ((Singleton? expression)
+    (let ((binding nil))
+    (if (Variable? expression)
+        (setq binding (LookUp expression s)))
+    (if (null binding)
+        expression
+    (Term binding))))
+    (t (cons (Substitute s (first expression))
+    (Substitute s (rest expression))))))
