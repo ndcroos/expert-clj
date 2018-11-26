7133b9138c0ba4014ee0c9ee3e1c28bfb6ced47c
Nick Decroos <ndcroos@users.noreply.github.com>
2018-11-26 21:23:06 +0100

    Update get-props.clj

--- src/expert_system_clj/get-props.clj
+++ src/expert_system_clj/get-props.clj
@@ -1,11 +1,14 @@
 ;;
-(defn Goal? [var]
-(eq (get var ’class) ’goal))
+(defn goal? 
+    [var]
+    (eq (get var ’class) ’goal))
 
 ;;
-(defn Traced? [var]
-(get var ’traced))
+(defn traced? 
+    [var]
+    (get var ’traced))
 
 ;;
-(defn GetPrompt [var]
-(get var ’prompt))
+(defn get-prompt 
+    [var]
+    (get var ’prompt))
