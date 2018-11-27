ba7b929268bf2c10c228e26720fd67c646b104e2
Nick Decroos <ndcroos@users.noreply.github.com>
2018-11-27 20:10:06 +0100

    Update substitute.clj

--- src/expert_system_clj/substitute.clj
+++ src/expert_system_clj/substitute.clj
@@ -4,7 +4,7 @@
         ((or (null expression)
         (null s)) expression)
     ((singleton? expression)
-    (let ((binding nil))
+    (let [(binding nil)]
     (if (variable? expression)
         (setq binding (lookUp expression s)))
     (if (null binding)
